a33a12f6c96dd3e4f6c6cb8abd61df8a347ad7f2
gabriel garcia <alu0100499377@ull.edu.es>
2013-11-01 18:30:29 +0000

    uno

---
+++ lib/prct07.rb
@@ -0,0 +1,167 @@
+class Fraccion
+   attr_accessor :num,:den
+
+   def initialize(num,den)
+      @num,@den = num,den
+   end
+
+   def show_frac
+      "(#{@num}/#{@den})"
+   end
+
+   def to_f
+      @num/@den.to_f
+   end
+
+   def reduc(num, den)	# Devuelve el maximo comun divisor
+		a = num.abs
+		b = den.abs
+		min = 1
+		k = 1
+		while (k <= a) and (k <= b) do
+			if ((a % k) == 0) and ((b % k) == 0) then
+				min = k
+			end
+			k += 1
+		end
+		return min
+	end
+
+	def simp      		# Simplifica la fraccion
+		
+		a = @num/reduc(@num, @den)
+		b = @den/reduc(@num, @den) 
+
+		@num,@den = a,b
+		return self
+	end
+
+	def == (other)
+		if @num == other.num and @den == other.den
+			return true
+		else
+			return false
+		end
+	end
+
+	def abs
+		a = @num.abs
+		b = @den.abs
+		return Fraccion.new(a,b).show_frac
+	end
+
+	def reciproco
+		a = @den
+		b = @num
+		return Fraccion.new(a,b).show_frac
+	end
+
+	def opuesto
+		if (@num<0 and @den>0) then
+		   a = @num *(-1) 
+		   b = @den
+		end
+	
+		if (@den<0 and @num>0) then
+	   	a = @num 
+	   	b = @den	*(-1)
+		end
+
+		if (@num>0 and @den>0) or (@num<0 and @den<0)then
+		   a = @num *(-1) 
+		   b = @den *(-1)
+		end
+
+		return Fraccion.new(a,b).show_frac
+	end
+
+	def + (other)
+		if (@den == other.den)
+			a = @num += other.num
+			b = @den
+		else
+			b = @den * other.den
+			a = ((b/@den) * @num) + ((b/other.den) * other.num)
+		end
+
+		return Fraccion.new(a, b).simp.show_frac
+	end
+
+	def - (other)
+		if (@den == other.den)
+			a = @num -= other.num
+			b = @den
+		else
+			b = @den * other.den
+			a = ((b/@den) * @num) - ((b/other.den) * other.num)
+		end
+
+		return Fraccion.new(a, b).simp.show_frac
+	end
+
+	def *(other)
+		num=@num*other.num
+		den=@den*other.den
+		return Fraccion.new(num,den).simp.show_frac
+	end
+
+	def /(other)
+		num=@num*other.den
+		den=@den*other.num
+		return Fraccion.new(num,den).simp.show_frac
+	end
+
+	def < (other)
+		if self.to_f < other.to_f
+			return true
+		else
+			return false
+		end
+	end
+
+	def > (other)
+		if self.to_f > other.to_f
+			return true
+		else
+			return false
+		end
+	end
+
+	def <=(other)
+		if (self.to_f) <= (other.to_f) then
+			true
+		else
+			false
+		end	
+	end
+	
+	def >=(other)
+		if (self.to_f) >= (other.to_f) then
+			true
+		else
+			false
+		end
+	end
+
+	#Modificacion. Multiplicar 2 fracciones y hallar el valor absoluto
+	def modif(other)
+		num=@num*other.num
+		den=@den*other.den
+		num=num.abs
+		den=den.abs
+		return Fraccion.new(num,den).simp.show_frac
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ spec/prct07_spec.rb~
@@ -0,0 +1,101 @@
+require 'rspec'
+require './lib/prac6'
+
+describe Fraccion do
+	
+	before :each do
+		@a = Fraccion.new(3,9)
+		@b = Fraccion.new(4,20)
+		@c = Fraccion.new(3,9)
+		@d = Fraccion.new(-3,-9)
+		@e = Fraccion.new(-4,20)
+	end
+
+	it "Debe existir un numerador" do
+		@a.num.should eq 3
+		@b.num.should eq 4
+	end
+
+	it "Debe existir un denominador" do
+		@a.den.should eq 9
+		@b.den.should eq 20
+	end
+
+	it "Debe estar en forma reducida" do
+		@a.simp.show_frac.should eq "(1/3)"
+		@b.simp.show_frac.should eq "(1/5)"
+	end
+
+	it "Debe mostrar la fraccion en la forma a/b" do
+		@a.show_frac.should eq "(3/9)"
+		@b.show_frac.should eq "(4/20)"
+	end
+
+	it "Debe mostrar la fraccion en formato float" do
+		@a.to_f.should eq 0.3333333333333333
+		@b.to_f.should eq 0.2
+	end
+
+	it "Debe mostrar la fraccion en formato float" do
+		@a.to_f.should eq 0.3333333333333333
+		@b.to_f.should eq 0.2
+	end
+
+	it "Debe comparar dos fracciones con ==" do
+		(@a==@b).should eq false
+		(@a==@c).should eq true
+	end
+
+	it "Debe calcular el valor absoluto" do
+		@d.abs.should eq "(3/9)"
+		@e.abs.should eq "(4/20)"
+	end
+
+	it "Debe calcular el reciproco" do
+		@a.reciproco.should eq "(9/3)"
+		@d.reciproco.should eq "(-9/-3)"
+	end
+
+	it "Debe calcular el opuesto" do
+		@c.opuesto.should eq "(-3/-9)"
+		@d.opuesto.should eq "(3/9)"
+		@e.opuesto.should eq "(4/20)"
+	end
+
+	it "Debe calcular la suma de 2 fracciones" do
+		(@a+@b).should eq "(8/15)"
+	end
+	
+	it "Debe calcular la resta de 2 fracciones" do
+		(@a-@b).should eq "(2/15)"
+	end
+	
+	it "Debe calcular la multiplicacion de 2 fracciones" do
+		(@a*@b).should eq "(1/15)"
+	end
+	
+	it "Debe calcular la division de 2 fracciones" do
+		(@a/@b).should eq "(5/3)"
+	end
+
+	it "Debe comparar si una fraccion es mayor que otra" do
+		(@a>@b).should eq true
+	end
+
+	it "Debe comparar si una fraccion es menor que otra" do
+		(@a<@b).should eq false
+	end
+
+	it "Debe comparar si una fraccion es mayor o igual que otra" do
+		(@a>=@b).should eq true
+	end
+
+	it "Debe comparar si una fraccion es menor o igual que otra" do
+		(@a<=@c).should eq true
+	end
+
+	it "Debe devolver el valor absoluto de la multiplicacion" do
+		@a.modif(@e).should eq "(1/15)"
+	end
+
+end
